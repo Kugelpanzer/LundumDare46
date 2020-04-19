556c83c639ea54435cd18ab6cc6a77d48ad5b70f
Kugelpanzer <djolemlad@gmail.com>
2020-04-19 16:16:26 +0200

    added rg

--- BirdMP/Assets/Scripts/CharacterScript.cs
+++ BirdMP/Assets/Scripts/CharacterScript.cs
@@ -14,7 +14,7 @@ public class CharacterScript : MonoBehaviourPun
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
