ef3b4853fdd9b597c3412ccacd22cb0aa71ba4e6
Kugelpanzer <djolemlad@gmail.com>
2020-04-19 02:25:00 +0200

    Photon init push

---
+++ BirdMP/Assets/Scripts/ConnectionController.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectionController : MonoBehaviourPunCallbacks
+{
+    public Button connectButton;
+    public InputField roomNameField;
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("CONNECTED TO " + PhotonNetwork.ServerSettingsFileName);
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.JoinRoom(roomNameField.text);
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(roomNameField.text, new Photon.Realtime.RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)2 });
+    }
+    public void OnConnectedToServer()
+    {
+        Debug.Log("CONNECTED TO SERVER ???");
+    }
+
+}
---
+++ BirdMP/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+//using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    public GameObject testPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Successfuly connected");
+        Debug.Log(PhotonNetwork.CloudRegion);
+        PhotonNetwork.AutomaticallySyncScene = true;
+        
+        PhotonNetwork.Instantiate("testPrefab",new Vector3(),new Quaternion());
+
+    }
+}
